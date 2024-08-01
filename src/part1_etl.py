3c6e0e23631d02776d1461382a0d0cb2dc081bda
Ajuani Hall <44127995+JayHall2002@users.noreply.github.com>
2024-08-01 17:39:07 -0400

    Update part1_etl.py
    create data directory

--- src/part1_etl.py
+++ src/part1_etl.py
@@ -10,6 +10,10 @@ import urllib
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def extract_and_save_data():
+    # Create the data directory if it does not exist
+    if not os.path.exists('../data'):
+        os.makedirs('../data')
+        
     pred_universe_raw = pd.read_csv('https://www.dropbox.com/scl/fi/69syqjo6pfrt9123rubio/universe_lab6.feather?rlkey=h2gt4o6z9r5649wo6h6ud6dce&dl=1')
     arrest_events_raw = pd.read_csv('https://www.dropbox.com/scl/fi/wv9kthwbj4ahzli3edrd7/arrest_events_lab6.feather?rlkey=mhxozpazqjgmo6qqahc2vd0xp&dl=1')
     pred_universe_raw['arrest_date_univ'] = pd.to_datetime(pred_universe_raw.filing_date)
