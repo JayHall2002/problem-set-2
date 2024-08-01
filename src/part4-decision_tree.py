f587a66b18d9b6be906ab2bfb6886ef8b056945f
Ajuani Hall <44127995+JayHall2002@users.noreply.github.com>
2024-08-01 17:12:16 -0400

    Update part4-decision_tree.py

--- src/part4-decision_tree.py
+++ src/part4-decision_tree.py
@@ -1,18 +1,27 @@
 '''
 PART 4: Decision Trees
-- Read in the dataframe(s) from PART 3
-- Create a parameter grid called `param_grid_dt` containing three values for tree depth. (Note C has to be greater than zero) 
-- Initialize the Decision Tree model. Assign this to a variable called `dt_model`. 
-- Initialize the GridSearchCV using the logistic regression model you initialized and parameter grid you created. Do 5 fold crossvalidation. Assign this to a variable called `gs_cv_dt`. 
-- Run the model 
-- What was the optimal value for max_depth?  Did it have the most or least regularization? Or in the middle? 
-- Now predict for the test set. Name this column `pred_dt` 
-- Return dataframe(s) for use in main.py for PART 5; if you can't figure this out, save as .csv('s) in `data/` and read into PART 5 in main.py
 '''
 
-# Import any further packages you may need for PART 4
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.model_selection import StratifiedKFold as KFold_strat
-from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+
+def run_decision_tree(df_arrests_train, df_arrests_test):
+    X_train = df_arrests_train[['current_charge_felony', 'num_fel_arrests_last_year']]
+    y_train = df_arrests_train['y']
+    X_test = df_arrests_test[['current_charge_felony', 'num_fel_arrests_last_year']]
+    y_test = df_arrests_test['y']
+
+    param_grid_dt = {'max_depth': [3, 5, 7]}
+    dt_model = DecisionTreeClassifier()
+    gs_cv_dt = GridSearchCV(dt_model, param_grid_dt, cv=5)
+    gs_cv_dt.fit(X_train, y_train)
+
+    print("Optimal value for max_depth:", gs_cv_dt.best_params_['max_depth'])
+    print("Did it have the most or least regularization? Or in the middle?")
+    # Print interpretation based on max_depth value
+
+    pred_dt = gs_cv_dt.predict(X_test)
+    df_arrests_test['pred_dt'] = pred_dt
+
+    return df_arrests_test, pred_dt
