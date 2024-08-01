a7cbd006d47e8257b1af9abc922987d915adc8b8
Ajuani Hall <44127995+JayHall2002@users.noreply.github.com>
2024-08-01 17:11:15 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,26 +3,36 @@ You will run this problem set from main.py, so set things up accordingly
 '''
 
 import pandas as pd
-import etl
-import preprocessing
-import logistic_regression
-import decision_tree
-import calibration_plot
+import part1_etl as etl
+import part2_preprocessing as preprocessing
+import part3_logistic_regression as logistic_regression
+import part4_decision_tree as decision_tree
+import part5_calibration_plot as calibration_plot
 
 
 # Call functions / instanciate objects from the .py files
 def main():
 
     # PART 1: Instanciate etl, saving the two datasets in `./data/`
+    etl.extract_and_save_data()
 
     # PART 2: Call functions/instanciate objects from preprocessing
+    df_arrests = preprocessing.preprocess_data()
 
     # PART 3: Call functions/instanciate objects from logistic_regression
+    df_arrests_train, df_arrests_test, pred_lr = logistic_regression.run_logistic_regression(df_arrests)
 
     # PART 4: Call functions/instanciate objects from decision_tree
+    df_arrests_test, pred_dt = decision_tree.run_decision_tree(df_arrests_train, df_arrests_test)
 
     # PART 5: Call functions/instanciate objects from calibration_plot
+    y_test = df_arrests_test['y']
+    calibration_plot.calibration_plot(y_test, df_arrests_test['pred_lr'], n_bins=5)
+    calibration_plot.calibration_plot(y_test, df_arrests_test['pred_dt'], n_bins=5)
 
+    # Print and analyze calibration results
+    print("Which model is more calibrated?")
+    # More code for PPV and AUC calculations for extra credit can go here
 
 if __name__ == "__main__":
     main()
