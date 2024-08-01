cbbb66f9bac14ee8cedd5525a12d033610aa1ba7
Ajuani Hall <44127995+JayHall2002@users.noreply.github.com>
2024-08-01 17:12:00 -0400

    Update part3-logistic_regression.py

--- src/part3-logistic_regression.py
+++ src/part3-logistic_regression.py
@@ -1,25 +1,27 @@
 '''
 PART 3: Logistic Regression
-- Read in `df_arrests`
-- Use train_test_split to create two dataframes from `df_arrests`, the first is called `df_arrests_train` and the second is called `df_arrests_test`. Set test_size to 0.3, shuffle to be True. Stratify by the outcome  
-- Create a list called `features` which contains our two feature names: pred_universe, num_fel_arrests_last_year
-- Create a parameter grid called `param_grid` containing three values for the C hyperparameter. (Note C has to be greater than zero) 
-- Initialize the Logistic Regression model with a variable called `lr_model` 
-- Initialize the GridSearchCV using the logistic regression model you initialized and parameter grid you created. Do 5 fold crossvalidation. Assign this to a variable called `gs_cv` 
-- Run the model 
-- What was the optimal value for C? Did it have the most or least regularization? Or in the middle? Print these questions and your answers. 
-- Now predict for the test set. Name this column `pred_lr`
-- Return dataframe(s) for use in main.py for PART 4 and PART 5; if you can't figure this out, save as .csv('s) in `data/` and read into PART 4 and PART 5 in main.py
 '''
 
-# Import any further packages you may need for PART 3
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.model_selection import StratifiedKFold as KFold_strat
-from sklearn.linear_model import LogisticRegression as lr
+from sklearn.linear_model import LogisticRegression
 
+def run_logistic_regression(df_arrests):
+    X = df_arrests[['current_charge_felony', 'num_fel_arrests_last_year']]
+    y = df_arrests['y']
 
-# Your code here
+    df_arrests_train, df_arrests_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=True, stratify=y)
 
+    param_grid = {'C': [0.01, 0.1, 1]}
+    lr_model = LogisticRegression()
+    gs_cv = GridSearchCV(lr_model, param_grid, cv=5)
+    gs_cv.fit(df_arrests_train, y_train)
 
+    print("Optimal value for C:", gs_cv.best_params_['C'])
+    print("Did it have the most or least regularization? Or in the middle?")
+    # Print interpretation based on C value
+
+    pred_lr = gs_cv.predict(df_arrests_test)
+    df_arrests_test['pred_lr'] = pred_lr
+
+    return df_arrests_train, df_arrests_test, pred_lr
